e64076b13d2816dabf57af2b3e9c25ae39741e76
N-meapp <developersnme@gmail.com>
2024-12-18 11:27:30 +0530

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+
+console.log("Hover effect initialized for boxes!");
+
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target); 
+        }
+    });
+}, {
+    threshold: 0.5 
+});
+
+
+
